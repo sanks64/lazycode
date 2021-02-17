06b1357f6559b3984c5c42dad7b54080ef69dbab
Andrei Zhigalkin <sanks64@protonmail.com>
2021-02-17 18:45:50 +0600

    Bump dependencies and pass the token with a keyword argument

--- src/lazycode.cr
+++ src/lazycode.cr
@@ -4,5 +4,5 @@ require "./lazycode/bot"
 require "./lazycode/config"
 
 config = LazyCode::Config.parse_file("src/config.yml")
-bot = LazyCode::Bot.new(config.token)
+bot = LazyCode::Bot.new(bot_token: config.token)
 bot.poll
